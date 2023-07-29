7cba47396895a060f503bd6ed3754a488a45ce90
Delowar Hossain <delowardev@gmail.com>
2023-07-30 00:32:13 +0600

    position styles added

--- demo/app.tsx
+++ demo/app.tsx
@@ -1,12 +1,20 @@
 import './app.css'
 // @ts-ignore
 import Alert from "../src/alert.tsx";
+import {Options} from "../src/types.ts";
 const alert = new Alert();
 
 
 const confirm = {
   text: 'Confirm',
   on: () => {},
+}
+
+const positionConfig: Options = {
+  type: "exclamatory",
+  title: "Hello world",
+  content: "This is our content",
+  confirm
 }
 
 export function App() {
@@ -31,16 +39,58 @@ export function App() {
     } );
     
   }
-
+  
+  const positions = [
+    [
+      'top-start',
+      'top',
+      'top-end'
+    ],
+    [
+      'center-start',
+      'center',
+      'center-end'
+    ],
+    [
+      'bottom-start',
+      'center',
+      'bottom-end'
+    ]
+  ];
+  
+  console.log( positions )
+  
   return (
-    <div className="demo">
-      <h1>AlertJS</h1>
-      <div class="card">
-        <button onClick={ onAlert }>Show Alert</button>
-        <br/>
-        <br/>
-        <button onClick={ onSuccess }>Show Success</button>
+      <div className="demo">
+        <h1>AlertJS</h1>
+        <div class="card">
+          <button onClick={ onAlert }>Show Alert</button>
+          <br/>
+          <br/>
+          <button onClick={ onSuccess }>Show Success</button>
+          
+          
+          <h3>Positions</h3>
+          <table>
+            <tbody>
+            {
+              positions.map( position => (
+                <tr>
+                  {
+                    position.map( _position => (
+                        <td>
+                          <button onClick={ alert.open.bind( null, { ...positionConfig, position: _position }) } children={ _position } />
+                        </td>
+                    ) )
+                  }
+                </tr>
+              ) )
+            }
+            </tbody>
+          
+          </table>
+        
+        </div>
       </div>
-    </div>
   )
 }
